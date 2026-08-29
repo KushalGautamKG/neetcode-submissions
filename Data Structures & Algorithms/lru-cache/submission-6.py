0dfcb34960e80c53af686281ed8a6b00e4676bbe
thisismyjam <136746105+KushalGautamKG@users.noreply.github.com>
2026-08-29 00:13:53 -0400

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.py
@@ -0,0 +1,75 @@
+class Node:
+
+
+    def __init__(self, key, value):
+
+        self.key = key
+        self.val = value
+
+        self.prev = self.next = None
+
+
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.cap = capacity
+        self.left = Node(0, 0)
+
+        self.right = Node(0, 0)
+
+
+        self.left.next = self.right
+        self.right.prev = self.left
+
+
+    def get(self, key: int) -> int:
+        
+
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+            self.insert(self.cache[key])
+
+            return self.cache[key].val
+
+
+
+        return -1
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+
+        prev.next = nxt
+        nxt.prev = prev
+
+
+    def insert(self, node):
+        prev = self.right.prev
+        prev.next = self.right.prev = node
+        node.next = self.right
+        node.prev = prev
+
+
+    def put(self, key: int, value: int) -> None:
+
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+
+
+        self.insert(self.cache[key])
+
+
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+
+            del self.cache[lru.key]
+
+
+
+
